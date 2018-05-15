d3f5ab9f79269c108c7078b5fdef21d5eff26064
brewencoded <brewencoded@gmail.com>
2018-05-14 19:44:15 -0500

    feat(Footer): added to Home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import Footer from './Footer/index'
 import Header from './Header/index'
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
